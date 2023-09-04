71f82f4e10f2ffe07516e6feeea1a0bade991304
Heet Patel <hiheet04@gmail.com>
2023-09-04 10:11:51 +0530

    texted

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "texted.hpp"
+
+int main( int argc , char **argv ){
+  if(argc > 1){
+    auto texted = std::make_shared<TextEd>(argv[1]);
+    texted->run();
+  }else{
+    auto texted = std::make_shared<TextEd>("");
+    texted->run();
+  }
+  return 0;
+}
